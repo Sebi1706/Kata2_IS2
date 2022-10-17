550931b5b2338b9fa7fba2df97e9976cff3c20a6
Sebi <Sebi@DESKTOP-MAD8HOE>
2022-10-17 17:54:08 +0200

    Third Commit

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,16 +7,13 @@ import java.util.Map;
 public class Kata2 {
 
     
+    
     public static void main(String[] args) {
         int [] data = {1,4,4,6,3,7,1,9,8,5,3,2,6,1,7};
         
-        Map <Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        
-        for (int i = 0; i< data.length; i++){
-            histogram.put(data[i],histogram.containsKey(data[i]) ? histogram.get(data[i]) + 1 : 1);
-            
-            
-        }
+        HistogramGenerator histogramGenerator = new HistogramGenerator(data);
+
+        Map<Integer, Integer> histogram = histogramGenerator.getHistogram();
         
         for (Map.Entry<Integer, Integer> entry : histogram.entrySet()){
             System.out.println(entry.getKey() + " ==> " + entry.getValue());
